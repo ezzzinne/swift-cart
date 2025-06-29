eb3dc9bdf53606a9fa640a887a6f5c24731b7f86
ezzzinne <cynthia.nwani017@gmail.com>
2025-06-29 23:22:26 +0100

    Implemented Header

---
+++ swift-cart-app/src/components/Header.tsx
@@ -0,0 +1,39 @@
+import Phone from "../assets/phone-icon.svg";
+import Mail from "../assets/mail-icon.svg";
+import Instagram from "../assets/instagram.svg";
+import Youtube from "../assets/youtube.svg";
+import Facebook from "../assets/facebook.svg";
+import Twitter from "../assets/twitter.svg";
+
+const Header = () => {
+    return (
+        <>
+            <section className="header-container">
+                <div className="first-item">
+                    <div className="item">
+                        <img src={Phone} alt="Phone icon" />
+                        <p>(225) 555-0118</p>
+                    </div>
+                    <div className="item">
+                        <img src={Mail} alt="Mail icon" />
+                        <p>michelle.rivera@example.com</p>
+                    </div>
+                </div>
+                <div className="second-item">
+                    <p>Follow Us  and get a chance to win 80% off</p>
+                </div>
+                <div className="third-item">
+                    <p>Follow Us  :</p>
+                    <div>
+                        <img src={Instagram} alt="Instagram icon" />
+                        <img src={Youtube} alt="Youtube icon" />
+                        <img src={Facebook} alt="Facebook icon" />
+                        <img src={Twitter} alt="Twitter icon" />
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Header;
