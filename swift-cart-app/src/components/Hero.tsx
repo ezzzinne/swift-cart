82bad3aa2487c1c1b53cd438759bb010e3e3f982
ezzzinne <cynthia.nwani017@gmail.com>
2025-06-30 00:14:51 +0100

    Implemented Hero section

---
+++ swift-cart-app/src/components/Hero.tsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+    return (
+        <section className="hero-container">
+            <div className="hero-overlay">
+                <h6 className="hero-subheading">Designing Better Experience</h6>
+                <h2 className="hero-title">
+                    Find Your Perfect Style
+                    <br />
+                    <span>Chairs, Watches, Headsets & More.</span>
+                </h2>
+                <p className="hero-description">
+                Premium quality products, unbeatable prices. <br /> Experience comfort, tech, and timeless fashion.
+                </p>
+                <button className="hero-button">Shop Now</button>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
